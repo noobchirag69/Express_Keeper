6c11ab88246e3d92176741ecc5b40832d6cc3675
noobchirag69 <chiragchakraborty48@gmail.com>
2022-07-16 15:13:30 +0530

    Designed the UI

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ app.set('view engine', 'ejs');
 app.listen(process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'The Dark Knight', snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'},
-        {title: 'The Man of Tomorrow', snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'},
-        {title: 'The Emerald Archer', snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'},
+    const notes = [
+        {title: 'The Dark Knight', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'},
+        {title: 'The Man of Tomorrow', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'},
+        {title: 'The Emerald Archer', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in.'},
     ];
-    res.render('index', { title: 'Home', blogs });
+    res.render('index', { title: 'Home', notes });
 });
 
-app.get('/about', (req, res) => {
-    res.render('about', { title: 'About' });
+app.get('/notes', (req, res) => {
+    res.redirect('/');
 });
 
-app.get('/blogs/create', (req, res) => {
+app.get('/create', (req, res) => {
     res.render('create', { title: 'Create' });
 });
 
